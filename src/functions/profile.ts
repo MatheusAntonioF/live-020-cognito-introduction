324c0a32c8c3649860063632014a361770dfdd95
Mat <matheusantoniof@gmail.com>
2026-07-21 10:13:34 -0300

    feat: retrieving user profile from cognito

--- src/functions/profile.ts
+++ src/functions/profile.ts
@@ -1,8 +1,19 @@
+import { cognitoClient } from "@/libs/cognitoClient";
 import { response } from "@/utils/response";
-import type { APIGatewayProxyEventV2 } from "aws-lambda";
+import { AdminGetUserCommand } from "@aws-sdk/client-cognito-identity-provider";
+import type { APIGatewayProxyEventV2WithJWTAuthorizer } from "aws-lambda";
 
-export async function handler(event: APIGatewayProxyEventV2) {
+export async function handler(event: APIGatewayProxyEventV2WithJWTAuthorizer) {
+  const userId = event.requestContext.authorizer.jwt.claims.sub as string;
+
+  const command = new AdminGetUserCommand({
+    Username: userId,
+    UserPoolId: process.env.COGNITO_USER_POOL_ID,
+  });
+
+  const { UserAttributes } = await cognitoClient.send(command);
+
   return response(200, {
-    profile: {},
+    profile: UserAttributes,
   });
 }
