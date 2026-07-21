a61d6f14026223831851e6e9c41e38526fa9fe3d
Mat <matheusantoniof@gmail.com>
2026-07-21 10:21:39 -0300

    feat: create endpoints to reset password

---
+++ src/functions/forgotPassword.ts
@@ -0,0 +1,23 @@
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
+import { ForgotPasswordCommand } from "@aws-sdk/client-cognito-identity-provider";
+
+import { bodyParser } from "@/utils/bodyParser";
+import { response } from "@/utils/response";
+import { cognitoClient } from "@/libs/cognitoClient";
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  try {
+    const { email } = bodyParser(event.body);
+
+    const command = new ForgotPasswordCommand({
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      Username: email,
+    });
+
+    await cognitoClient.send(command);
+
+    return response(204);
+  } catch (error) {
+    return response(500, { error: "Internal server error" });
+  }
+}
